8ef1605d068a36d9ea298a6dbd2535cc193c4d40
Yuriy <the.yurec1@yandex.ru>
2022-10-28 21:48:58 +0300

    add slider

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 $(function () {
- 
+
   $(".rateYo").rateYo({
     rating: 4.5,
-    readOnly: true,
- 
+    readOnly: true
+    })
+
+  $(".reviews__slider").slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 2,
+    draggable: false,
+    variableWidth: true,
+    appendArrows: $('.reviews__slider-arrows'),
+    appendDots: $('.reviews__slider-dots')
+  })
+
+  $('.reviews__slider-prev').on('click', function(e){
+    e.preventDefault()
+      $('.reviews__slider').slick('slickPrev')
+  })
+  $('.reviews__slider-next').on('click', function(e){
+    e.preventDefault()
+      $('.reviews__slider').slick('slickNext')
+  })
+
 });
