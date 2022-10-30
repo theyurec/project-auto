5f76c231dc1621c7f6540bd44ac95ac5bb2ca4bb
Yury <yurius_1@yahoo.com>
2022-10-30 19:17:41 +0300

    added footer and mobile header

--- js/main.js
+++ js/main.js
@@ -37,4 +37,19 @@ $(function () {
     }
   })
 
+  $('.burger, .overlay').on('click', function(e) {
+    e.preventDefault()
+    $('.header__top').toggleClass('header__top--open')
+    $('.overlay').toggleClass('overlay--show')
+    $('.burger').toggleClass('burger--close')
+  })
+
+  setInterval(() => {
+    if($(window).scrollTop() > 0 && $('.header__top').hasClass('header__top--open') === false) {
+      $('.burger').addClass('burger--follow')
+    } else {
+      $('.burger').removeClass('burger--follow')
+    }  
+  }, 0)
+
 });
